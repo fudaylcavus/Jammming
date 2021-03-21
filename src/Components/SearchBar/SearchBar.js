9eab79adcfe256d87b31cd4e7db143912c39426f
fudaylcavus <fudaylcavus@gmail.com>
2021-03-21 14:07:33 +0300

    enter to search

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -7,6 +7,7 @@ class SearchBar extends React.Component {
         this.state = { value: ''}
         this.search = this.search.bind(this)
         this.handleTermChange = this.handleTermChange.bind(this)
+        this.handleSearch = this.handleSearch.bind(this)
         
     }
 
@@ -19,10 +20,16 @@ class SearchBar extends React.Component {
         this.setState({ value: event.target.value})
     }
 
+    handleSearch(event) {
+        if (event.key === "Enter")
+            this.search()
+    }
+
     render() {
         return (
             <div className="SearchBar">
-                <input 
+                <input
+                onKeyPress={this.handleSearch} 
                 onChange={this.handleTermChange}
                 placeholder="Enter A Song, Album, or Artist" />
                 <button
